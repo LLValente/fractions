03db35ba5694d8251e1f98398e64fd75e7b82f22
lucas675lv@gmail.com <lucas675lv@gmail.com>
2023-05-29 12:26:42 -0300

    Módulo questions quase terminado. Mundanças no módulo sql_questions

--- questions.py
+++ questions.py
@@ -5,25 +5,97 @@ import os
 
 
 class Question:
-
-    def __init__(self, question_type):
-
-        #self.start = QUESTIONS_START[self.type]
-        #self.end = QUESTIONS_START[self.type]
-        self.type = question_type
-        self.items = self.items(Questions_dict[self.type][items_number])
-        self.answears = ''
+    # Alterar questões existentes
 
 
+    def __init__(self, id_question='', question_type=''):
 
-    def items(self, items_numbers):
-    # Cria as fraçãoes a serem utilizadas em uma questão (em alguns tipos de questões, deverá construir o dobro do esperado)
+        def verify_id(id_question):
+
+            conection_info =    ("Driver={SQL Server};"
+                                "Server=DESKTOP-EKI2AGF\SQLEXPRESS;"
+                                "Database=SQL_Exams_DB;")
+
+            conection = pyodbc.connect(conection_info)
+
+            cursor = conection.cursor()
+
+            cursor.execute("SELECT * FROM Questions;")
+
+            rows = cursor.fetchall()
+
+            for row in rows:
+
+                if id_question in row:
+
+                    self.id = row[0]
+                    self.subject = row[1]
+                    self.type = row[2]
+                    self.difficulty = row[3]
+                    self.header = row[4]
+                    self.pontuation = row[5]
+                    self.double_items = row[6]
+                    self.items_number = row[7]
+                    self.ending = row[8]
+                    self.id_competence = row[9]
+
+                    return True
+
+            return False
+
+
+        def register_question(self, question_type=''):
+
+            if question_type == '':
+
+                question_type = input('Qual o tipo da questão?')
+
+            self.id = 'frac' + question_type.lower()[-1:-4]
+            self.subject = 'Fractions'
+            self.type = question_type
+            self.difficulty = input('Qual o nível de dificuldade da questão? (1, 2 ou 3):')
+            self.header = input('Qual o cabeçalho da questão? (Em string):')
+            self.pontuation = input('Quanto vale a questão? (Em decimais):')
+            self.double_items = input('Duplicar items? (0 ou 1):')
+            self.items_number = input('Qual a quantidade de items na questão? (Em int):')
+            self.ending = input('Qual o final da questão? (Em string):')
+            self.id_competence = input('Qual a competência da BNCC? (Em string):')
+
+            conection_info =    ("Driver={SQL Server};"
+                                "Server=DESKTOP-EKI2AGF\SQLEXPRESS;"
+                                "Database=SQL_Exams_DB;")
+
+            conection = pyodbc.connect(conection_info)
+
+            cursor = conection.cursor()
+
+            command = f"""INSERT INTO Questions (id_question, question_subject, question_type, question_difficulty, question_header, pontuation, double_items, items_number, question_ending, id_competence, register_date, register_user)
+            VALUES ('{self.id}', '{self.subject}', '{self.type}', {self.difficulty}, '{self.header}', {self.pontuation}, {self.double_items}, {self.items_number}, '{self.ending}', '{self.id_competence}', '{str(datetime.datetime.now())[:-7]}', '{os.getlogin()}')"""
+
+            cursor.execute(command)
+            cursor.commit()
+
+
+        if not verify_id(id_question):
+
+            register_question(question_type)
+
+
+    def items(self):
+
+        if self.double_items == 1:
+
+            items_number = 2 * self.items_number
+
+        elif self.double_items == 0:
+
+            items_number = self.items_number
 
         fractions = []
 
         i = 1
 
-        while i <= items_numbers:
+        while i <= items_number:
 
             fraction = frac.Fraction()
             fractions.append(fraction)
@@ -33,26 +105,27 @@ class Question:
         return fractions
 
     
-    def write(self):
-    # Deverá escrever a questão em LaTeX
+    def write_question_in_latex(self):
 
-        pass
+        fractions = self.items()
 
+        latex_header = "\t" + r"\item " + self.header + r" \textbf{(" + str(self.pontuation) + r" pontos)}" + "\n"
+        latex_items_block = "\t\t" + r"\begin{enumerate}" + "\n"
+        latex_items = ''
 
-def sign_in_question(dict):
+        for fraction in fractions:
 
-    conection_info = (
-        "Driver={SQL Server};"
-        "Server=DESKTOP-EKI2AGF\SQLEXPRESS;"
-        "Database=SQL_Exams_DB;"    
-    )
+            latex_item = "\t\t\t" + r"\item $\frac{" + str(fraction.numerator) + "}{" + str(fraction.denominator) + "}$" + "\n" # ou r"\item \frac{}{} e \frac{}{}***"
 
-    conection = pyodbc.connect(conection_info)
+            latex_items = latex_items + latex_item
 
-    cursor = conection.cursor()
+        if self.ending == '':
 
-    command = f"""INSERT INTO Questions (id_question, question_subject, question_type, question_difficulty, question_header, pontuation, double_items, items_number, question_ending, id_competence, register_date, register_user)
-    VALUES ('{dict['id_question']}', '{dict['question_subject']}', '{dict['question_type']}', '{dict['question_difficulty']}', '{dict['question_header']}', '{dict['pontuation']}', '{dict['double_items']}', '{dict['items_number']}', '{dict['question_ending']}', '{dict['id_competence']}', '{dict['register_date']}', '{dict['register_user']}')"""
+            latex_ending = "\t\t" + r"\end{enumerate}"
+        
+        else:
 
-    cursor.execute(command)
-    cursor.commit()
+            latex_ending = "\t" + self.ending + "\n\t\t" + r"\end{enumerate}"
+
+        print(latex_header + latex_items_block +  latex_items + latex_ending)
+
--- sql_questions.py
+++ sql_questions.py
@@ -6,7 +6,7 @@ import os
 
 def main():
 
-    Drawing=    {'id_question': 'fracdraw0101',
+    Drawing=    {'id_question': 'fracdraw01',
                 'question_subject': 'Fractions',         #varchar(20) NOT NULL,
                 'question_type': 'Drawing',            #varchar(15) NOT NULL,
                 'question_difficulty': 1,      #int NOT NULL,
@@ -19,7 +19,7 @@ def main():
                 'register_date': str(datetime.datetime.now())[:-7],            #datetime NOT NULL,
                 'register_user': os.getlogin()}            #varchar(15) NOT NULL}
 
-    Reading=    {'id_question': 'fracread0101',
+    Reading=    {'id_question': 'fracread01',
                 'question_subject': 'Fractions',         #varchar(20) NOT NULL,
                 'question_type': 'Reading',            #varchar(15) NOT NULL,
                 'question_difficulty': 1,      #int NOT NULL,
@@ -32,7 +32,7 @@ def main():
                 'register_date': str(datetime.datetime.now())[:-7],            #datetime NOT NULL,
                 'register_user': os.getlogin()}            #varchar(15) NOT NULL}
 
-    Comparing=  {'id_question': 'fraccomp0101',
+    Comparing=  {'id_question': 'fraccomp01',
                 'question_subject': 'Fractions',         #varchar(20) NOT NULL,
                 'question_type': 'Comparing',            #varchar(15) NOT NULL,
                 'question_difficulty': 1,      #int NOT NULL,
@@ -45,7 +45,7 @@ def main():
                 'register_date': str(datetime.datetime.now())[:-7],            #datetime NOT NULL,
                 'register_user': os.getlogin()}            #varchar(15) NOT NULL}
 
-    Conversion= {'id_question': 'fracconv0101',
+    Conversion= {'id_question': 'fracconv01',
                 'question_subject': 'Fractions',         #varchar(20) NOT NULL,
                 'question_type': 'Conversion',            #varchar(15) NOT NULL,
                 'question_difficulty': 1,      #int NOT NULL,
@@ -56,31 +56,63 @@ def main():
                 'question_ending': '',          #varchar(8) NOT NULL,
                 'id_competence': 'EF08MA05',            #varchar(10) NOT NULL,
                 'register_date': str(datetime.datetime.now())[:-7],            #datetime NOT NULL,
-                'register_user': os.getlogin()}                  #varchar(15) NOT NULL}
+                'register_user': os.getlogin()}                  #varchar(15) NOT NULL
 
 
     def sign_in_question(dict):
 
-        conection_info = (
-            "Driver={SQL Server};"
-            "Server=DESKTOP-EKI2AGF\SQLEXPRESS;"
-            "Database=SQL_Exams_DB;"    
-        )
+        conection_info =    ("Driver={SQL Server};"
+                            "Server=DESKTOP-EKI2AGF\SQLEXPRESS;"
+                            "Database=SQL_Exams_DB;")
 
         conection = pyodbc.connect(conection_info)
 
         cursor = conection.cursor()
+
+        try:
+
+            command =   """ALTER TABLE Questions DROP CONSTRAINT fk_id_competence;
+                        ALTER TABLE Exams DROP CONSTRAINT fk_id_question;
+                        ALTER TABLE Exams DROP CONSTRAINT fk_id_competence2;
+
+                        DROP TABLE Questions
+
+                        CREATE TABLE Questions
+                        (
+                            id_question varchar(12) PRIMARY KEY NOT NULL,
+                            question_subject varchar(20) NOT NULL,
+                            question_type varchar(15) NOT NULL,
+                            question_difficulty int NOT NULL,
+                            question_header varchar(500) NOT NULL,
+                            pontuation float NOT NULL,
+                            double_items int NOT NULL,
+                            items_number int NOT NULL,
+                            question_ending varchar(8) NOT NULL,
+                            id_competence varchar(10) NOT NULL,
+                            register_date datetime NOT NULL,
+                            register_user varchar(15) NOT NULL,
+                        );
+
+                        ALTER TABLE Questions ADD CONSTRAINT fk_id_competence FOREIGN KEY (id_competence) REFERENCES BNCC (id_competence)
+                        ALTER TABLE Exams ADD CONSTRAINT fk_id_question FOREIGN KEY (id_question) REFERENCES Questions (id_question)
+                        ALTER TABLE Exams ADD CONSTRAINT fk_id_competence2 FOREIGN KEY (id_competence) REFERENCES BNCC (id_competence)
+                        """
+
+            cursor.execute(command)
+            cursor.commit()
+
+        except: return False
+
+
+    dicts = [Drawing, Reading, Comparing, Conversion]
+
+    for dict in dicts:
 
         command = f"""INSERT INTO Questions (id_question, question_subject, question_type, question_difficulty, question_header, pontuation, double_items, items_number, question_ending, id_competence, register_date, register_user)
         VALUES ('{dict['id_question']}', '{dict['question_subject']}', '{dict['question_type']}', '{dict['question_difficulty']}', '{dict['question_header']}', '{dict['pontuation']}', '{dict['double_items']}', '{dict['items_number']}', '{dict['question_ending']}', '{dict['id_competence']}', '{dict['register_date']}', '{dict['register_user']}')"""
 
         cursor.execute(command)
         cursor.commit()
-
-
-    dicts = [Drawing, Reading, Comparing, Conversion]
-
-    for dict in dicts:
 
         sign_in_question(dict)
 
